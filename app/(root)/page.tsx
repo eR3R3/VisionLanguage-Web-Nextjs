8c73a8d0503ab9331a00d9510aa4abf0f0627959
eR3R3 <er1r1@qq.com>
2024-11-14 13:50:32 +0800

    Basic parts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,27 @@
+'use client'
+
 import Image from "next/image";
 import {UserButton} from "@clerk/nextjs";
 import SideBar from "@/components/shared/SideBar";
 import {createUser} from "@/lib/actions/user.actions";
+import {connectToDatabase} from "@/lib/database/mongoose";
 
 
 const Home = () => {
+    const handleClick = () =>{
+        console.log(process.env.NEXT_PUBLIC_MONGODB_URL)
+        console.log(process.env.WEBHOOK_SECRET)
+    }
+
+    const handleCreateUser = () => {
+
+    }
   return (
     <div>
       <p>Home</p>
       <UserButton/>
+        <button onClick={connectToDatabase}>connect</button>
+        <button onClick={handleClick}>PrintMongodbUrl</button>
     </div>
   );
 }
--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,6 +1,9 @@
-import mongoose, {Connection, Mongoose} from 'mongoose';
+'use server'
 
-const MONGODB_URL = process.env.MONGODB_URL;
+import mongoose, {Mongoose} from 'mongoose';
+
+const MONGODB_URL: string = process.env.MONGODB_URL!;
+console.log('MongoDB connection url: ', MONGODB_URL);
 
 interface MongooseConnection {
     connection: Mongoose|null;
@@ -12,10 +15,10 @@ export const connectToDatabase = async() => {
     if(cache.connection) return cache.connection;
     if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
     if(!cache.promise){
-        cache.promise = mongoose.connect(MONGODB_URL, {dbName: 'Vision AI', bufferCommands: false})
+        cache.promise = mongoose.connect(MONGODB_URL, {dbName: 'visionAI', bufferCommands: false})
     }
     cache.connection = await cache.promise
-    console.log("connect to db")
+    console.log("connect to DB successfully")
     return cache.connection
 }
 
