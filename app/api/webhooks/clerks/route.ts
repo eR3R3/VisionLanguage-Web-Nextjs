9c792193736058a793e6bc4d009099dc04205923
eR3R3 <er1r1@qq.com>
2024-11-14 22:47:42 +0800

    Basic parts

--- app/api/webhooks/clerks/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET(req: Request) {
-    console.log('Received a GET request at /api/hello');
-    return new Response(
-        JSON.stringify({ message: 'Hello, this is a test API!' }),
-        { status: 200 }
-    );
-}
